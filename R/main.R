92d385585b100851009d533f2799cf56bc23ec07
gongcastro <gonzalo.garciadecastro@upf.edu>
2022-01-21 23:27:34 +0100

    First commit

--- R/main.R
+++ R/main.R
@@ -2,20 +2,24 @@ library(here)
 library(readxl)
 library(dplyr)
 library(tidyr)
+library(lubridate)
 library(stringr)
 library(janitor)
 
 # import data
 df <- read_xlsx(here("data", "raw", "zombies.xlsx")) %>% 
     clean_names() %>% 
-    filter(type %in% c("Film", "Series")) %>% 
+    filter(
+        type %in% c("Film", "Series"),
+        year <= year(Sys.Date())
+        ) %>% 
     mutate(
         author = str_replace(author, "/", ", "), 
         type = str_replace(type, "Series", "TV Show")
     ) %>%
     select(title, type, year, author) %>% 
     arrange(-year) %>% 
-    drop_na()
+    drop_na(year)
     
 
 write.csv(df, "zombie-movies.csv", row.names = FALSE)
